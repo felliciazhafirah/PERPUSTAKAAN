bb300a82036762974448587780d0ec7b9d5546f4
Fellicia Yoja Zhafirah <felliciayoja15@gmail.com>
2026-02-11 10:08:42 +0700

    fixing loans dto new

--- src/loans/dto/create-loan.dto.ts
+++ src/loans/dto/create-loan.dto.ts
@@ -3,13 +3,10 @@ import { IsNumber, IsDateString } from 'class-validator';
 export class BorrowBookDto {
   @IsNumber()
   memberId: number;
-
   @IsNumber()
   bookId: number;
-
   @IsDateString()
   borrowDate: string;
-
   @IsDateString()
   dueDate: string;
 }
